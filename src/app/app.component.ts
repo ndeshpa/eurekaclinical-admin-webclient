62d646a2cec68954c852f3bb3db220f6909dc865
Nita <Nita@BMI-Post-XPS1.bmi.emory.edu>
2018-01-09 14:35:05 -0500

    Timeout implemented as in analytics-webclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,87 +9,116 @@ import { Router } from '@angular/router';
     styleUrls: ['./app.component.css']
 } )
 export class AppComponent implements OnInit, OnDestroy {
-    
+
     sessSubscription: Subscription;
     logoutSubscription: any;
     timeOut: boolean = false;
     sessionTimeout: any;
     currentUrl: any;
     timer: any;
+    finalTimer: any;
     sessionStartTime: any;
-    
-    constructor(private adminService: AdminService, private router: Router) {
-        
-     }
-    
+    graceSecs = 150;
 
-    getSessionProperties(){
+    constructor( private adminService: AdminService, private router: Router ) {
+
+    }
+
+
+    getSessionProperties() {
         //get current time and save in localStorage
         this.sessSubscription = this.adminService.getSessionProperties().subscribe( data => {
             var sessTimeout = JSON.stringify( data );
             JSON.parse( sessTimeout, ( key, value ) => {
-                localStorage.setItem(key,value);
-            });
-        });
+                localStorage.setItem( key, value );
+            } );
+        } );
     }
 
 
-    ngOnInit() {  
+    ngOnInit() {
         this.getSessionProperties();
-        this.sessionTimeout = +localStorage.getItem('maxInactiveInterval') - 180;
+        this.sessionTimeout = +localStorage.getItem( 'maxInactiveInterval' ) - 180;
         //this.sessionTimeout = 10; //for testing timeout
-        this.timerReset(this.sessionTimeout);      
-      }
-    
-  
-    doTimedout(){
+        //this.graceSecs = 5; //for testing timeout
+        this.timerReset( this.sessionTimeout );
+    }
+
+
+    doTimedout() {
+        this.timeOut = false;
+        //console.log( 'logging out' );
         this.logoutSubscription = this.adminService.doLogout();
         this.logoutSubscription.subscribe( myData => {
-            console.log('logged out');
-        });
-        this.router.navigate(['/welcome']);
+            console.log( 'logged out' );
+        } );
+        this.router.navigate( ['/welcome', 'loggedOut'] );
     }
     
-    @HostListener('document:mousemove') onMouseMove() { 
-        localStorage.setItem('timeOut', 'false');
-        this.timerReset(this.sessionTimeout);
-      }
-    
-    @HostListener('document:click') onClick() { 
-        localStorage.setItem('timeOut', 'false');
-        this.timerReset(this.sessionTimeout);
-      }
-    
-    @HostListener('document:keyup') onKeyUp() { 
-        localStorage.setItem('timeOut', 'false');
-        this.timerReset(this.sessionTimeout);
-      }
-    
-    @HostListener('document:keydown') onKeyDown() { 
-        localStorage.setItem('timeOut', 'false');
-        this.timerReset(this.sessionTimeout);
-      }
+    graceTimeout(){
+        this.finalTimer = setTimeout(() => {
+            //console.log('grace period ends');
+            this.doTimedout();
+        }, this.graceSecs * 1000 );
+    }
+
+    @HostListener( 'document:mousemove' ) onMouseMove() {
+        localStorage.setItem( 'timeOut', 'false' );
+        this.timerReset( this.sessionTimeout );
+    }
+
+    @HostListener( 'document:click' ) onClick() {
+        localStorage.setItem( 'timeOut', 'false' );
+        this.timerReset( this.sessionTimeout );
+    }
+
+    @HostListener( 'document:keyup' ) onKeyUp() {
+        localStorage.setItem( 'timeOut', 'false' );
+        this.timerReset( this.sessionTimeout );
+    }
+
+    @HostListener( 'document:keydown' ) onKeyDown() {
+        localStorage.setItem( 'timeOut', 'false' );
+        this.timerReset( this.sessionTimeout );
+    }
     //give 2 minutes advance warning
-    timerReset(timeoutSecs: any){
-        if(this.timer !== null)
-            clearTimeout(this.timer);
-        localStorage.setItem('timeOut', 'false');
-        if(!this.router.url.endsWith('timeout')){
+    timerReset( timeoutSecs: any ) {
+        if ( this.timer !== null )
+            clearTimeout( this.timer );
+        if (this.finalTimer !== null)
+            clearTimeout(this.finalTimer);
+        localStorage.setItem( 'timeOut', 'false' );
+        if ( !this.router.url.endsWith( 'timeout' ) ) {
             this.currentUrl = this.router.url;
-            localStorage.setItem('currUrl', this.currentUrl);
-        }   
-        if(!this.router.url.endsWith('loggedOut')){
-            this.timer = setTimeout(() => {
-                localStorage.setItem('timeOut', 'true');
-                this.router.navigate(['/timeout']);
-            }, timeoutSecs*1000);
+            localStorage.setItem( 'currUrl', this.currentUrl );
         }
-        
+        if ( !this.router.url.endsWith( 'loggedOut' ) ) {
+            this.timer = setTimeout(() => {
+                //console.log('starting timeout period');
+                this.timeOut = true;
+                localStorage.setItem( 'timeOut', 'true' );
+                //console.log('starting grace period');
+                this.graceTimeout();
+            }, timeoutSecs * 1000 );
+        }
     }
-    
+
+    resetSession() {
+        //console.log( 'extending session' );
+        if ( this.timer !== null )
+            clearTimeout( this.timer );
+        if (this.finalTimer !== null)
+            clearTimeout(this.finalTimer);
+        localStorage.setItem( 'timeOut', 'false' );
+        this.timeOut = false;
+        this.getSessionProperties();
+        //console.log( 'currUrl: ' + localStorage.getItem( 'currUrl' ) );
+        this.router.navigateByUrl( localStorage.getItem( 'currUrl' ) );
+    }
+
     ngOnDestroy(): void {
-        if(this.sessSubscription !== null)
+        if ( this.sessSubscription !== null )
             this.sessSubscription.unsubscribe();
     }
- 
+
 }
