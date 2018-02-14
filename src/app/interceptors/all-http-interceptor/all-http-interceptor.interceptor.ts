dad9192925c4014a5f39b17870e9ce5a2ebd8484
Nita <Nita@BMI-Post-XPS1.bmi.emory.edu>
2018-02-14 14:17:16 -0500

    fix to useHash

--- src/app/interceptors/all-http-interceptor/all-http-interceptor.interceptor.ts
+++ src/app/interceptors/all-http-interceptor/all-http-interceptor.interceptor.ts
@@ -5,29 +5,35 @@ import { Router } from '@angular/router';
 
 @Injectable()
 export class AllHttpInterceptor implements HttpInterceptor {
-    constructor(private router:Router){}
-  intercept (request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      console.log('Inside Interceptor:' + request.url);
-      return next.handle(request)
-      .do(event => {
-        if (event instanceof HttpResponse) {
-            console.log(event);
-            if (event.url.indexOf('webclient') >= 0 ||
-                    event.status === 302){
-                console.log('------------GOT ADMINVIEW--------------');
-                localStorage.setItem('isNewUser', 'false');
-                localStorage.setItem( 'loggedIn', 'true' );
-                this.router.navigate['/adminview'];
-            }
-            if (event.url.indexOf('logout') >= 0){
-                localStorage.setItem('isNewUser', 'true');
-            }
-        }
-      }, err => {
-        console.log('Caught error', err.url);
-        if(err.url.indexOf('adminview') >= 0){
-            this.router.navigate(['/adminview']);
-        }
-      });
-  }
+    constructor( private router: Router ) { }
+    intercept( request: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
+        console.log( 'Inside Interceptor:' + request.url );
+        return next.handle( request )
+            .do( event => {
+                if ( event instanceof HttpResponse ) {
+                    console.log( event );
+                    if ( event.headers.get( 'loggedIn' ) === 'true' ) {
+                        console.log( '------------GOT ADMINVIEW--------------' );
+                        localStorage.setItem( 'isNewUser', 'false' );
+                        localStorage.setItem( 'loggedIn', 'true' );
+                        this.router.navigate['/adminview'];
+                    }
+                    if ( event.url.indexOf( 'logout' ) >= 0 ) {
+                        localStorage.setItem( 'isNewUser', 'true' );
+                    }
+                }
+                else {
+                    console.log( event );
+                    console.log( '------------GOT ADMINVIEW--------------' );
+                    localStorage.setItem( 'isNewUser', 'false' );
+                    localStorage.setItem( 'loggedIn', 'true' );
+                    this.router.navigate['/adminview'];
+                }
+            }, err => {
+                console.log( 'Caught error', err.url );
+                if ( err.url.indexOf( 'adminview' ) >= 0 ) {
+                    this.router.navigate( ['/adminview'] );
+                }
+            } );
+    }
 }
