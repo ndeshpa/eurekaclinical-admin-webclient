1a1c55efd53f71eaab7018903a9cefd3c0677b5a
nimit <nimit@172.24.252.208>
2018-07-20 11:22:10 -0400

    Added angular component to display all jobs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,7 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
 import { AdminviewComponent } from './components/adminview/adminview.component';
 import { EditUserComponent } from './components/edit-user/edit-user.component';
 import { UserAgreementComponent } from './components/useragreement/useragreement.component';
+import { JobViewComponent } from './components/job-view/job-view.component';
 
 
 
@@ -35,6 +36,7 @@ const appRoutes: Routes = [
     { path: 'logout', component: AdminviewComponent },
     { path: 'editUser/me/:action/:id', component: EditUserComponent },
     { path: 'editUser/:action/:id', component: EditUserComponent },
+    { path: 'jobView', component: JobViewComponent },
     { path: '', redirectTo: '/welcome', pathMatch: 'full' }
 ];
 
@@ -62,7 +64,8 @@ const appRoutes: Routes = [
         WelcomeComponent,
         AdminviewComponent,
         EditUserComponent,
-        UserAgreementComponent
+        UserAgreementComponent,
+    	JobViewComponent
     ],
     exports: [
         RouterModule
---
+++ src/app/components/job-view/job-view.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
+import { Subscription } from 'rxjs/Rx';
+import { Job } from '../../models/job';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+
+@Component({
+    selector: 'app-job-view',
+    templateUrl: './job-view.component.html',
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: ['./job-view.component.css']
+})
+
+export class JobViewComponent implements OnInit {
+    displayedColumns = ['id', 'sourceConfigId', 'created', 'user', 'destination', 'name', 'finished'];
+    subscription: Subscription;
+    public jobs: Job[] = [];
+    dataSource: MatTableDataSource<Job>;
+    public page: number = 1;
+    public itemsPerPage: number = 10;
+    public maxSize: number = 5;
+    public numPages: number = 1;
+    public length: number = 0;
+    public paging = true;
+    errorMsg: string = '';
+    data: any;
+    constructor(private adminService: AdminService) { }
+
+    ngOnInit() {
+        this.jobs[0] = {'id':1, 'sourceConfigId':'Spreadsheet', 'created': new Date(), 
+                        'user':1, 'destination':1, 'name':'Job1', 'finished': new Date()};
+        this.jobs[1] = {'id':2, 'sourceConfigId':'Spreadsheet', 'created': new Date(), 
+                        'user':1, 'destination':1, 'name':'Job1', 'finished': new Date()};
+        this.dataSource = new MatTableDataSource<Job>(this.jobs);
+        this.adminService.getAllJobs().subscribe(data => {
+            this.data = data;
+            for (var i = 0; i < this.data.length; i++) {
+                this.jobs[i] = {
+                    'id': this.data[i].id,
+                    'sourceConfigId': this.data[i].sourceConfigId,
+                    'created': this.data[i].created,
+                    'user': this.data[i].user,
+                    'destination': this.data[i].destination,
+                    'name': this.data[i].name,
+                    'finished': this.data[i].finished
+                };
+            }
+
+        },
+            error => {
+                if (error instanceof HttpErrorResponse) {
+                    this.errorMsg = 'Server Error: ' + error.message;
+                }
+                else {
+                    this.errorMsg = 'Error Running Query, Please Retry';
+                }
+            },
+            () => {
+                console.log('SUCCESS in JOB');
+            });
+    }
+
+    public changePage(page: any, data: Job[] = this.sortData()) {
+
+        let start = (page.page - 1) * page.itemsPerPage;
+        let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
+        return new MatTableDataSource<Job>(data.slice(start, end));
+    }
+
+    public onChangeTable(page: any = { page: this.page, itemsPerPage: this.itemsPerPage }): any {
+        this.dataSource = page && this.paging ? this.changePage(page, this.sortData())
+            : new MatTableDataSource<Job>(this.sortData());
+    }
+
+    sortData() {
+        return this.jobs.sort((a, b) => {
+            if (a.user < b.user) return -1;
+            else if (a.user > b.user) return 1;
+            else return 0;
+        });
+    }
+
+    applyFilter(filterValue: string) {
+        filterValue = filterValue.trim();
+        filterValue = filterValue.toLowerCase();
+        this.dataSource.filter = filterValue;
+    }
+}
